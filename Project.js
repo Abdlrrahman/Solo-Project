777eec4bdafbb9d8d7db876c2e0866575354f296
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2019-11-07 23:01:33 +0200

    6

--- Project.js
+++ Project.js
@@ -1,8 +1,18 @@
-function game(userChoice) {
-	var rock = 'rock';
-	var paper = 'paper';
-	var scissors = 'scissors';
+function game() {
+	var move = "";
+	$('#rock').click(function() {
+		move = 'rock';
+	$("#result").html('You chose is ' + move + '.');
+	});
+	$('#paper').click(function() {
+		move = 'paper';
+			$("#result").html('You chose is ' + move + '.');
+	});
+	$('#scissors').click(function() {
+		move = 'scissors';
+			$("#result").html('You chose is ' + move + '.');
+	});
 	var loses = 0;
 	var wins = 0;
-
 }
+game();
