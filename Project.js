064d109bf54f9b543a9e97603acf00b74ffb5448
fatima <onlyfefe03@gmail.com>
2019-11-08 16:33:24 +0100

    15

--- Project.js
+++ Project.js
@@ -1,4 +1,4 @@
-function game() {
+$(document).ready(function game() {
 	var loses = 0;
 	var wins = 0;
 	var userChoice = "";
@@ -6,13 +6,14 @@ function game() {
 
 	$("#result").html(wins + ' - ' + loses);
 	$('.moves').click(function() {
-		userChoice = $(this).attr('value');;
-		setTimeout(function(){$("#userResult").html('Your chose is ' + userChoice + '.')}, 0);
-		setTimeout(function(){$("#computerResult").html("The computer's chose is " + opponent(computerChoice) + '.')}, 1000);
-		setTimeout(function(){$("#finalresult").html(compare(userChoice,opponent(computerChoice)))}, 2000);
-		setTimeout(function(){$("#result").html(wins + ' - ' + loses)}, 3000);
+		userChoice = $(this).attr('value');
+		setTimeout(function(){$("#userResult").html('Your chose is ' + userChoice + '.');$('.moves').attr('disabled', true);}, 0);
+		setTimeout(function(){$("#computerResult").html("The computer's chose is " + opponent(computerChoice) + '.')}, 500);
+		setTimeout(function(){$("#finalresult").html(compare(userChoice,opponent(computerChoice)))}, 1000);
+		setTimeout(function(){$("#result").html(wins + ' - ' + loses);$('.moves').attr('disabled', false);}, 1500);
 		computerChoice = Math.random();
 	});
+
 
 	var opponent = function(computerChoice) {
 		if (computerChoice < 0.34) {
@@ -30,14 +31,17 @@ function game() {
 	} else if (choice1 == "rock"){
 		if (choice2 == "scissors") {
 			wins++;
+			$('#imgRock').toggle();;
 			return "rock wins.";
 		} else {
+			$('#imgRock').toggle();;
 			loses++;
 			return "sorry. paper wins.";
 		}
 	} else if (choice1 == "paper") {
 		if (choice2 == "rock") {
 			wins++;
+			$('#imgRock').toggle();;
 			return "paper wins";
 		} else {
 			loses++;
@@ -65,5 +69,5 @@ $('#reset').click(function() {
 	$("#result").html(wins + ' - ' + loses);
 })
 	
-}
+});
 game();
