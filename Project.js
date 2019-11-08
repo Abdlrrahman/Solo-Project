6c80fa802546f062f46fe769cb63f6ed8c719ef4
fatima <onlyfefe03@gmail.com>
2019-11-08 12:16:20 +0100

    10

--- Project.js
+++ Project.js
@@ -3,26 +3,11 @@ function game() {
 	var wins = 0;
 	var userChoice = "";
 	var computerChoice = Math.random();
+	var choices = ['rock', 'paper', 'scissors'];
 
 	$("#result").html(wins + ' - ' + loses);
-	$('#rock').click(function() {
-		userChoice = 'rock';
-		$("#userResult").html('Your chose is ' + userChoice + '.');
-		$("#computerResult").html("The computer's chose is " + opponent(computerChoice) + '.');
-		$("#finalresult").html(compare(userChoice,opponent(computerChoice)));
-		$("#result").html(wins + ' - ' + loses);
-		computerChoice = Math.random();
-	});
-	$('#paper').click(function() {
-		userChoice = 'paper';
-		$("#userResult").html('Your chose is ' + userChoice + '.');
-		$("#computerResult").html("The computer's chose is " + opponent(computerChoice) + '.');
-		$("#finalresult").html(compare(userChoice,opponent(computerChoice)));
-		$("#result").html(wins + ' - ' + loses);
-		computerChoice = Math.random();
-	});
-	$('#scissors').click(function() {
-		userChoice = 'scissors';
+	$('.moves').click(function() {
+		userChoice = $(this).attr('value');;
 		$("#userResult").html('Your chose is ' + userChoice + '.');
 		$("#computerResult").html("The computer's chose is " + opponent(computerChoice) + '.');
 		$("#finalresult").html(compare(userChoice,opponent(computerChoice)));
@@ -43,24 +28,24 @@ function game() {
 	function compare(choice1,choice2) {
 	if (choice1 == choice2) {
 		return "The result is a tie!";
-	} else if (choice1 === "rock"){
-		if (choice2 === "scissors") {
+	} else if (choice1 == "rock"){
+		if (choice2 == "scissors") {
 			wins++;
 			return "rock wins.";
 		} else {
 			loses++;
 			return "sorry. paper wins.";
 		}
-	} else if (choice1 === "paper") {
-		if (choice2 === "rock") {
+	} else if (choice1 == "paper") {
+		if (choice2 == "rock") {
 			wins++;
 			return "paper wins";
 		} else {
 			loses++;
 			return "sorry. scissors win.";
 		}
-	} else if (choice1 === "scissors") {
-		if (choice2 === "rock") {
+	} else if (choice1 == "scissors") {
+		if (choice2 == "rock") {
 			loses++;
 			return "sorry. rock wins";
 		} else {
